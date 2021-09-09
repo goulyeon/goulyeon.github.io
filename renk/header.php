bd048d1fc598cce2c2cf3b39805792cb0ca6f250
goulyeon <52442755+goulyeon@users.noreply.github.com>
2021-09-09 15:18:10 +0900

    renk

---
+++ renk/header.php
@@ -0,0 +1,15 @@
+<div id="header">
+    <h1><a href="/index.html"><img src="../../img/logo.gif" alt="리엔케이"></a></h1>
+    <img class="menu" src="../../img/menu.jpg" alt="메뉴">
+    <img class="close" src="../../img/close.jpg" alt="메뉴 닫기">
+    <ul>
+        <li><a href="story.html">STORY</a></li>
+        <li><a href="product.html">PRODUCT</a></li>
+        <li><a href="comunity.html">COMUNITY</a></li>
+        <li><a href="join.html">회원가입</a></li>
+        <li><a href="login.html">로그인</a></li>
+        <li><a href="mypage.html">마이페이지</a></li>
+        <li><a href="membership.html">멤버쉽</a></li>
+        <li class="no_m"><a href="/g5/bbs/board.php?bo_table=free">게시판</a></li>
+    </ul>
+</div>
