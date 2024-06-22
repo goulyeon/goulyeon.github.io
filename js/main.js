96b2307f75ddc039d3badb8487dec4c80a901850
goulyeon <52442755+goulyeon@users.noreply.github.com>
2024-06-22 21:58:23 +0900

    240622

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
 
     // 별 반짝이는 효과
     function stars(){
+
         const count = 80;
         const section = document.querySelector('.home');
         var i = 0;
@@ -45,10 +46,8 @@ $(document).ready(function(){
     stars();
 
     // 메뉴 클릭시 이동,스타일변경
-    $(".gnb ul li").click(function(){
+    $(".center .gnb ul li").click(function(){
 
-        $(".gnb ul li").removeClass("on");
-        $(this).addClass("on");
         
         var gnbIndex = $(this).index();
 
@@ -57,7 +56,9 @@ $(document).ready(function(){
         },100);
 
     });
-    
+
+
+
     $(window).scroll(function() {
 
         // 스크롤 시 메뉴 컬러변경
@@ -73,28 +74,56 @@ $(document).ready(function(){
 
         // 스크롤 시 위치에 따라 메뉴 스타일변경
         if (scrollH >= $('.home').offset().top){
-            $(".gnb ul li").removeClass("on");
-            $(".gnb ul li").eq(0).addClass("on");
+            $(".center .gnb ul li").removeClass("on");
+            $(".center .gnb ul li").eq(0).addClass("on");
+            
+            $(".menu-mo .gnb ul li").removeClass("on");
+            $(".menu-mo .gnb ul li").eq(0).addClass("on");
         }
         if (scrollH >= $('.about').offset().top){
-            $(".gnb ul li").removeClass("on");
-            $(".gnb ul li").eq(1).addClass("on");
+            $(".center .gnb ul li").removeClass("on");
+            $(".center .gnb ul li").eq(1).addClass("on");
+            
+            $(".menu-mo .gnb ul li").removeClass("on");
+            $(".menu-mo .gnb ul li").eq(1).addClass("on");
+
         }
         if (scrollH >= $('.portfolio').offset().top){
-            $(".gnb ul li").removeClass("on");
-            $(".gnb ul li").eq(2).addClass("on");
+            $(".center .gnb ul li").removeClass("on");
+            $(".center .gnb ul li").eq(2).addClass("on");
+            
+            $(".menu-mo .gnb ul li").removeClass("on");
+            $(".menu-mo .gnb ul li").eq(2).addClass("on");
         }
         if (scrollH >= $('.contact').offset().top){
-            $(".gnb ul li").removeClass("on");
-            $(".gnb ul li").eq(3).addClass("on");
+            $(".center .gnb ul li").removeClass("on");
+            $(".center .gnb ul li").eq(3).addClass("on");
+            
+            $(".menu-mo .gnb ul li").removeClass("on");
+            $(".menu-mo .gnb ul li").eq(3).addClass("on");
         }
 
     });
 
 
-
+    $(".menu-mo .gnb .menu").click(function(){
+        $(".menu-mo").addClass("on")
+    });
+    $(".menu-mo .gnb .close").click(function(){
+        $(".menu-mo").removeClass("on")
+    });
     
 
+    // 메뉴 클릭시 이동,스타일변경 mo
+    $(".menu-mo .gnb ul li").click(function(){
+
+        var gnbIndex = $(this).index();
+
+        $('html,body').animate({
+            scrollTop:$('section').eq(gnbIndex).offset().top
+        },100);
+
+    });
 
 
 
